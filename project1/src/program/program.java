4bfa47451c81914a8396a03ea4573c14474f23ff
dhavalg832 <dhavalg832@gmail.com>
2017-07-20 22:41:00 +0530

    this is the first project

---
+++ project1/src/program/program.java
@@ -0,0 +1,22 @@
+package program;
+import java.util.Scanner;
+/**
+ * Created by dhavald832gmail.com on 12-06-2017.
+ */
+public class program {
+    public static void main(String[] args) {
+        System.out.println("how many names you want to enter ");
+
+        Scanner s1 = new Scanner(System.in);
+        int n = s1.nextInt();
+
+
+        for(i=0;i<50;i++)
+        {
+
+            System.out.println("Enter your name");
+            name[i]=s1.next();
+
+        }
+    }
+}
